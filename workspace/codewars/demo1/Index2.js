14726ff178958f3526cef63cb214674aa2e3710b
waaagMeng <654946869@qq.com>
2019-04-05 10:54:42 +0800

    'ajax'

---
+++ workspace/codewars/demo1/Index2.js
@@ -0,0 +1,11 @@
+function arrmin(arr){
+   var min=arr[0];
+   for(let i=0;i<arr.length;i++){
+       if(arr[i]<min){
+           min=arr[i];
+       }
+   }
+   return min;
+}
+var thearray=[34,15,88,2];
+console.log(arrmin(thearray));
