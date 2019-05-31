22355b587d930e19a68eb3b49a5230960e82ca81
waaagMeng <654946869@qq.com>
2019-05-31 17:02:19 +0800

    'vue'

---
+++ workspace/vue/vue-router/routerLearn/src/components/LookOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="look-one">{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: 'LookOne',
+    data () {
+        return {
+            msg: '你瞅啥？'
+        }
+    }
+}
+</script>
---
+++ workspace/vue/vue-router/routerLearn/src/components/LookTwo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="look-two">{{msg}}
+        <div class="go-one" @click='goOne'>去Look1</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LookTwo',
+    data () {
+        return {
+            msg: '瞅你咋地！'
+        }
+    },
+    methods: {
+        goOne () {
+            this.$router.push({ name: 'LookOne',param: {id:123}})
+        }
+    },
+    beforeRouteEnter (to, from, next) {
+        console.log(from,to)
+        next()
+    },
+    beforeRouteLeave (to, from, next) {
+        console.log(from,to)
+        next()
+    }
+}
+</script>
