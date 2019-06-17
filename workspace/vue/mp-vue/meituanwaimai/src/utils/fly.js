9ae03a6cc1f9090d888807e0801ce56f17ae6a76
waaagMeng <654946869@qq.com>
2019-06-17 17:02:21 +0800

    'vue'

---
+++ workspace/vue/mp-vue/meituanwaimai/src/utils/fly.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+const Fly = require('flyio/dist/npm/wx.js');
+const fly = new fly();
+// /dasouche/api/v2/get
+// /dasouche/api/v2/set
+// /dasouche/api/v2/update
+fly.config.baseURL ="https://www.easy-mock.com/mock/5aded45053796b38dd26e970/"
+
+Vue.prototype.$http = fly;
